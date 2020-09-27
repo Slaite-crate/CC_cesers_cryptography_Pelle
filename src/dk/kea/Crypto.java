af43ce1b152b1ed70488f1768df123c6f0a0886f
Pelle <pelle.park@gmail.com>
2020-09-27 11:35:02 +0200

    hello

--- src/dk/kea/Crypto.java
+++ src/dk/kea/Crypto.java
@@ -11,29 +11,6 @@ public class Crypto {
         this.letterList = letterManager.getLetterList();
     }
 
-    private String shifter(int shift, String message){
-        if (shift < 0){
-            shift = letterList.size() - ((shift * -1) % letterList.size());
-        }
-        StringBuilder result = new StringBuilder();
-        for (char c : message.toCharArray()){
-            char ch = c;
-            for (int i = 0; i < letterList.size(); i++){
-                if (c == letterList.get(i)){
-                    for (int j = 0; j < letterList.size(); j++){
-                        if (j == (i + shift) % letterList.size()){
-                            ch = letterList.get(j);
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-            result.append(ch);
-        }
-        return result.toString();
-    }
-
     public String enCrypt(int shift, String message){
         return shifter(shift, message);
     }
@@ -42,5 +19,27 @@ public class Crypto {
         return shifter(-shift, message);
     }
 
+    private String shifter(int shift, String message){
+        if (shift < 0){
+            shift = letterList.size() - ((shift * -1) % letterList.size());
+        }
+        StringBuilder result = new StringBuilder();
+        for (char c : message.toCharArray()){
+            result.append(shiftedChar(c, shift));
+        }
+        return result.toString();
+    }
 
+    private char shiftedChar(char c, int shift){
+        for (int i = 0; i < letterList.size(); i++){
+            if (c == letterList.get(i)){
+                for (int j = 0; j < letterList.size(); j++){
+                    if (j == (i + shift) % letterList.size()){
+                        return letterList.get(j);
+                    }
+                }
+            }
+        }
+        return c;
+    }
 }
